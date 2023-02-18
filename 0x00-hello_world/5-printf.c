ac33c30afb31a6351a2cb30417939e519b998ed9
Tawaz29 <nyahwatawanda@gmail.com>
2023-02-18 06:55:02 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #incude < stdio.h >
 /**
- * main - A program that prints line using printf
- * Return: 0
+ * main - A program that prints line using printf function
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
